ae0cdaa61491b3d4dc8c448b79586d1ad2753bdf
ArKaNeMaN <arkaneman1@gmail.com>
2021-12-25 13:24:48 +0300

    Remove birthday column from players table

--- src/components/Players/PlayersListItem.vue
+++ src/components/Players/PlayersListItem.vue
@@ -3,7 +3,6 @@
 		<td>{{ player.id }}</td>
 		<td>{{ player.nickname }}</td>
 		<td>{{ player.name }}</td>
-		<td>{{ player.birthday ? utils.fmtDate(player.birthday) : 'Не указана' }}</td>
 		<td>
 			<router-link class="btn btn-outline-info d-block" :to="'/player/' + player.id">
 				Подробнее
